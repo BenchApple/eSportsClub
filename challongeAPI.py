0c57b6d50337b0f8f80282619b1fe8791f4f0e01
ImDaNoob <bjchappell02@icloud.com>
2019-04-25 19:48:15 -0400

    added create matches matrix and get matches methodolgy

---
+++ challongeAPI.py
@@ -0,0 +1,6 @@
+# Benjamin Chappell
+
+import pychallonge
+
+def getKey():
+    keyFile = open("../apikey.txt", "r")
--- eloCalc.py
+++ eloCalc.py
@@ -2,6 +2,7 @@
 
 import collections
 
+# Initializes the list of names.
 def initNames():
     # Open the file that stores all of the names in order, and assign it to an array.
     nameFile = open("names.txt", "r+")
@@ -14,9 +15,7 @@ def initNames():
     # Return the list of names as well as the file object that accesses the names.
     return [names, nameFile]
 
-def updateNames(nameFile, names):
-    nameFile.truncate(0)
-
+# Initializes the list of ELOs
 def initElo():
     eloFile = open("elo.txt", "r+")
     elos = eloFile.readlines()
@@ -28,11 +27,31 @@ def initElo():
     # Return the list of elos as well as the file object that accesses the elos.
     return[elos, eloFile]
 
-def initializeStorage():
-    nameFile = open("names.txt", "r+")
-    eloFile = open("elo.txt", "r+")
+# Creates a matrix of the matches that happened in a given tourney. Make sure to enter the matches in chronological order. 
+# The first number entered is the winner of the match. Reference the list of names to see the indices of each person.
+# If I can figure out a better way to do this in the future I most certainly will use that.
+# The matches are read in as a collection, aka a point, where the first one is known as winner and the second one is known as loser.
+def createMatchesMatrix(matches, names):
+    # Create a matrix of zeros to store the matches
+    matchMatrix = [[0 for j in range(0,len(names))] for i in range(0, len(names))]
 
-    elo = eloFile.readlines()
-    names = nameFile.readlines()
+    for match in matches:
+        matchMatrix[match.winner][match.loser] += 1
 
-initElo()
+    return matchMatrix
+
+def getMatches():
+    matches = []
+    Point = collections.namedtuple("Point", ["winner", "loser"])
+
+def main():
+    # Get initialized name stuff.
+    name = initNames()
+    names = name[0]
+    nameFile = name[1]
+
+    # Get initialized elo stuff.
+    elo = initElo()
+    elos = elo[0]
+    eloFile = elo[1]
+
